b035b90e2833433b0cf52212dacb0b45ac3377c0
Hailey Francis <n01402670@unf.edu>
2022-12-01 19:54:54 -0500

    edits, trying to get idea of how to display each question

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,15 @@
  * Last Edited: 12/1/22
  */
 
+var introEls = $("#intro").children();
 var startButton = $("#start-button");
 var timerEl = $("#timer");
-var questionDisplay = $("display-question");
+var questionForm = $("#question-form");
 
-var question = $("#question");
-var option1 = $("option1");
-var option2 = $("option2");
-var option3 = $("option3");
-var option4 = $("option4");
+var option1 = $("#option1");
+var option2 = $("#option2");
+var option3 = $("#option3");
+var option4 = $("#option4");
 
 //timeLeft is outside of timer function so that it can be reduced in other functions (primarily when a question is answered incorrectly)
 var timeLeft = 300; //starting with 300s (5min), README didn't specify time limit
@@ -23,6 +23,8 @@ var timeLeft = 300; //starting with 300s (5min), README didn't specify time limi
  */
 function startQuiz(){
     startTimer();
+    introEls.toggle();
+    displayQuestion();
     //display a question and its options
     //once question is answered, move on to next question
         //if question is wrong, subtract from timer
@@ -53,29 +55,41 @@ function startTimer(){
  * note: haven't tested yet
  */
 function incorrect(){
+    console.log("incorrect function begin execute");
     if(timeLeft > 10){
         timeLeft = timeLeft - 10;
     }else{
         timeLeft = 0;
     }
+    console.log("incorrect function end execute");
 }
 
-function displayQuestion(event){
+function displayQuestion(){
     //questions will likely be displayed in the same order every time. 
     //If randomizing the order is trivial, might change
-    event.preventDefault();
-    
+    //for each loop TODO
+        var question = q0.question;
+        var o1 = q0.c1;
+        var o2 = q0.c2;
+        var o3 = q0.c3;
+        var o4 = q0.c4;
+        var answer = q0.answer;
+
+        questionForm.append("<h3>" + question + "</h3>");
+        questionForm.append("<button type=\"button\" id=\"option1\">" + o1 + "</button>");
+        questionForm.append("<button type=\"button\" id=\"option2\">" + o2 + "</button>");
+        questionForm.append("<button type=\"button\" id=\"option3\">" + o3 + "</button>");
+        questionForm.append("<button type=\"button\" id=\"option4\">" + o4 + "</button>");
 
 }
 
-function answerQuestion(event){
-    event.preventDefault();
-
-
+function checkAnswer(){
+    
 }
 
 
 startButton.on("click", startQuiz);
+option1.on("click", incorrect);
 
 
 //going to make questions object constants for now
